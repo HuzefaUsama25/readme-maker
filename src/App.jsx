af559cbf19aa8c14f5ba065bbc52c33da06d9772
HuzefaUsama25 <huzefausama25@gmail.com>
2022-03-15 18:52:00 +0500

    Implemented basic styles

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar';
+
+const App = () => {
+	return (
+		<div className="App beauty-box">
+			<Navbar />
+
+			<section className="builder beauty-box">
+				<section className="Work beauty-box">
+					<h1>Hello, World</h1>
+				</section>
+
+				<section className="Result beauty-box">
+					<h1>Hello, World again</h1>
+				</section>
+			</section>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+	return (
+		<nav>
+			<ul className="burger-menu ">BM</ul>
+
+			<ul className="main-nav beauty-box">
+				<li>ReadmeMaker</li>
+				<li>
+					<a href="mailto:huzefausama@gmail.com">Leave Feedback</a>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
